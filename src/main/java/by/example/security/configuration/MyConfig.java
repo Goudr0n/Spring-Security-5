0b09acd8a4d7640a511f38331fd705bc6adb24ce
Евгений Аксёнов <liquid.dll@gmail.com>
2025-08-17 21:02:46 +0300

    Хранение паролей в БД

--- src/main/java/by/example/security/configuration/MyConfig.java
+++ src/main/java/by/example/security/configuration/MyConfig.java
@@ -1,11 +1,14 @@
 package by.example.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "by.example.security")
@@ -19,5 +22,21 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
 
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource;
+        try {
+            dataSource = new ComboPooledDataSource();
+            dataSource.setDriverClass("com.mysql.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/spring_course_db2?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return dataSource;
     }
 }
